c7722a81caa74eb0f545af9d70e8348184c1d902
SofiaFei <feisofiafei@gmail.com>
2023-04-09 16:37:10 +0700

    feat: add decoration on landing Page

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Document</title>
-  @vite('resources/css/app.css')
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TrashPorter</title>
+    @vite('resources/css/app.css')
 </head>
+
 <body>
-      <h1 class="text-3xl font-bold underline bg-black">Hello world!</h1>
+      <div class="px-64 py-6">
+            <div class="nav">
+                  <img src="assets/img/blob.svg" class="absolute right-0 top-0" alt="">
+            </div>
+
+      </div>
 </body>
+
 </html>
