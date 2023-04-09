241504236ab9266041b9b24c4dd5e505c60b4ddc
fatiyaquzza <fatiyaquzzaaa@gmail.com>
2023-04-09 13:02:29 +0700

    feat: template form login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,32 +1,49 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="h-100vh bg-gradient-to-r from-cyan-500 to-blue-500">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-           
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                <div class="w-24 mx-auto flex justify-center">
-                    <a href="/">
-                        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                    </a>
-                </div>
-                
-                {{ $slot }}
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="h-100vh bg-[url('assets/img/bg2.svg')]">
+
+    <header class="absolute w-10 py-2 mx-4 hover:animate-pulse">
+        <a href=""><img src="assets/img/back-arrow.png" alt="gambar"></a>
+    </header>
+
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-2 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+
+        <div class="flex justify-center pt-10">
+            <div class="mx-6 justify-center">
+                <h2 class="font-bold text-3xl w-full tracking-tight text-white">TrashPorter</h2>
             </div>
+
         </div>
-    </body>
+        <div class="flex justify-center mt-1 px-6">
+            <p class="text-white"> Reduce, reuse, and recycle to prevent further damage to our environment</p>
+        </div>
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-24 mx-auto flex justify-center">
+                <a href="/">
+                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                </a>
+            </div>
+
+            {{ $slot }}
+        </div>
+    </div>
+</body>
+
 </html>
--- storage/framework/views/da79366a4b442e92c75810fecc685f73.php
+++ storage/framework/views/da79366a4b442e92c75810fecc685f73.php
@@ -1,27 +1,43 @@
 <!DOCTYPE html>
 <html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-        <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
 
-        <!-- Scripts -->
-        <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
-    </head>
-    <body class="h-100vh bg-gradient-to-r from-cyan-500 to-blue-500">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-           
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                <div class="w-24 mx-auto flex justify-center">
-                    <a href="/">
-                        <?php if (isset($component)) { $__componentOriginal71c6471fa76ce19017edc287b6f4508c = $component; } ?>
+    <!-- Scripts -->
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+</head>
+
+<body class="h-100vh bg-[url('assets/img/bg2.svg')]">
+
+    <header class="absolute w-10 py-2 mx-4 hover:animate-pulse">
+        <a href=""><img src="assets/img/back-arrow.png" alt="gambar"></a>
+    </header>
+
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-2 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+
+        <div class="flex justify-center pt-10">
+            <div class="mx-6 justify-center">
+                <h2 class="font-bold text-3xl w-full tracking-tight text-white">TrashPorter</h2>
+            </div>
+
+        </div>
+        <div class="flex justify-center mt-1 px-6">
+            <p class="text-white"> Reduce, reuse, and recycle to prevent further damage to our environment</p>
+        </div>
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-24 mx-auto flex justify-center">
+                <a href="/">
+                    <?php if (isset($component)) { $__componentOriginal71c6471fa76ce19017edc287b6f4508c = $component; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.application-logo','data' => ['class' => 'w-20 h-20 fill-current text-gray-500']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
 <?php $component->withName('application-logo'); ?>
 <?php if ($component->shouldRender()): ?>
@@ -36,13 +52,13 @@
 <?php $component = $__componentOriginal71c6471fa76ce19017edc287b6f4508c; ?>
 <?php unset($__componentOriginal71c6471fa76ce19017edc287b6f4508c); ?>
 <?php endif; ?>
-                    </a>
-                </div>
-                
-                <?php echo e($slot); ?>
-
+                </a>
             </div>
+
+            <?php echo e($slot); ?>
+
         </div>
-    </body>
-</html>
-<?php /**PATH /home/fatiya/Documents/Trashporter/INF206-2023-DY1-TrashPorter/resources/views/layouts/guest.blade.php ENDPATH**/ ?>
+    </div>
+</body>
+
+</html><?php /**PATH /home/fatiya/Documents/Trashporter/INF206-2023-DY1-TrashPorter/resources/views/layouts/guest.blade.php ENDPATH**/ ?>
