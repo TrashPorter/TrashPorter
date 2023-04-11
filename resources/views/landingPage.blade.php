9149500b898ee0b165762ecab0e2a1cec6bd4264
SofiaFei <feisofiafei@gmail.com>
2023-04-11 09:27:58 +0700

    feat: decor landing page

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -23,7 +23,7 @@ dark:bg-neutral-800
     flex
     items-center
     justify-between
-    pb-4
+    pb-3
     border-b-2
     ">
             <a href="/"
@@ -61,7 +61,7 @@ dark:bg-neutral-800
         hover:text-neutral-600
         transition-colors
         "
-            aria-expanded="false" aria-label="Open Menu">
+                aria-expanded="false" aria-label="Open Menu">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
@@ -151,7 +151,7 @@ dark:bg-neutral-800
                     class="
                 py-2
                 px-6
-                bg-sky-800
+                bg-blue-500
                 text-white
                 shadow-xl
                 hover:shadow-none
@@ -167,36 +167,47 @@ dark:bg-neutral-800
         </div>
     </nav>
 
-    <header class="relative pt-16 md:pt-24 lg:pt-48">
-        <div
-            class="
-    absolute
-    inset-0
-    bottom-8
-    md:bottom-24
-    xl:bottom-32
-    -z-10
-    ">
-        <div class="container
-        mx-auto
-        grid
-        grid-rows-1
-        place-items-end
-        px-2">
-            <img src="assets/img/trashcan.png" 
-            class="
-            row-start-1
-            row-end-2
-            col-start-1
-            col-end-2
-            mx-auto
-            " 
-            alt="trashcan">
+    <main class="flex">
+        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+          </div>
+        {{-- <div class="
+            flex
+            absolute
+            inset-0
+            bottom-8
+            md:bottom-24
+            xl:bottom-32
+            -z-10
+            "> --}}
+        <div class="
+                    m-auto
+                w-1/3
+                {{-- sm:w-3/4 sm:h-3/4
+                md:w-full md:h-full --}} ">
+            <img src="assets/img/trashcan.png" alt="trashcan" />
         </div>
-        <div>
-            
+        <div class="mt-44 mr-72 text-center">
+            <h1 class="font-bold text-4xl tracking-tight text-gray-900 sm:text-6xl">Antar Jemput Sampah</h1>
+            <p class="mt-6 text-lg leading-8 text-gray-600">Mengelola sampah membuat kita lebih menyayangi lingkungan</p>
+
+            <button
+                class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-md mt-12">
+                Join Us
+            </button>
         </div>
-    </header>
+        {{-- </div>Antar Jemput Sampah
+            Mengelola sampah membuat kita lebih
+            menyayangi lingkungan --}}
+            <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true">
+            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+            </div>
+        </div>
+    </main>
+
+    
     <script type="module" src="assets/js/main.js"></script>
 </body>
 
