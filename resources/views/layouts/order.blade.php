f3712b63fae65205f985a574ae7c52e0d956d8aa
fatiyaquzza <fatiyaquzzaaa@gmail.com>
2023-04-13 15:52:15 +0700

    feat : add file order

---
+++ resources/views/layouts/order.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Order</title>
+    <script src="order.js"></script>
+</head>
+
+<body>
+    <nav class="h-18 w-full p-4 flex justify-between border-b shadow-sm">
+        <div class="flex px-5">
+            <img src="assets/img/logo.PNG" class="h-auto w-36">
+        </div>
+        <div class="mr-8">
+            <h2 class="font-semibold pt-4">Pesan Antar</h2>
+        </div>
+    </nav>
+
+</body>
+
+</html>
