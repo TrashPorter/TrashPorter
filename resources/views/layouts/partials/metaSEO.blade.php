185d4fd5f509c747a08d1e25e63b17442db4d046
SofiaFei <feisofiafei@gmail.com>
2023-04-14 01:54:12 +0700

    feat: add meta SEO

---
+++ resources/views/layouts/partials/metaSEO.blade.php
@@ -0,0 +1,7 @@
+ <!-- Meta SEO -->
+ <meta name="title" content="Landwind - Tailwind CSS Landing Page">
+ <meta name="description" content="Get started with a free and open-source landing page built with Tailwind CSS and the Flowbite component library.">
+ <meta name="robots" content="index, follow">
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ <meta name="language" content="English">
+ <meta name="author" content="Themesberg">
