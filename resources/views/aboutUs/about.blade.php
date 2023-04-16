49b9ffefa4ab865349abbffd5beed79e9d15db34
SofiaFei <feisofiafei@gmail.com>
2023-04-16 13:07:01 +0700

    feat: add about us section

--- resources/views/aboutUs/about.blade.php
+++ resources/views/aboutUs/about.blade.php
@@ -2,5 +2,17 @@
 
 @section('container')
     @include('layouts.partials.header')
-    <h1 class="pt-20 text-sky-500 text-center">SOPAN</h1>
+<section class="pt-20">   
+    <div class="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+        <div class="flex flex-col lg:flex-row justify-between gap-8">
+            <div class="w-full lg:w-5/12 flex flex-col justify-center">
+                <h1 class="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 dark:text-white pb-4">About Us</h1>
+                <p class="font-normal text-base leading-6 text-gray-600 dark:text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+            </div>
+            <div class="w-full lg:w-8/12">
+                <img class="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
+            </div>
+        </div>
+    </div>
+</section> 
 @endsection
