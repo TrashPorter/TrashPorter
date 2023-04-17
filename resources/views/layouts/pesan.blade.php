7d1a69020a45c0098729a3e1da3a6888c73b3388
fatiyaquzza <fatiyaquzzaaa@gmail.com>
2023-04-17 23:54:11 +0700

    feat : change style page pesan

--- resources/views/layouts/pesan.blade.php
+++ resources/views/layouts/pesan.blade.php
@@ -22,7 +22,7 @@
         </div>
     </nav>
 
-    <h1 class="w-1/2 mt-6 ml-20 font-semibold text-2xl">
+    <h1 class="w-1/2 mt-6 ml-20 font-semibold text-2xl mb-8">
         New Order
     </h1>
     <form action="" method="post" class=" ml-4 drop-shadow-xl rounded-lg max-sm:">
@@ -34,9 +34,9 @@
                 <p>Isi form dibawah untuk melakukan pemesanan</p>
             </div> --}}
 
-            <fieldset class="pb-8 border-b-2">
+            <fieldset class="pb-8 border-b-2 mx-8">
 
-                <div class="relative z-0 w-full mb-6 group ml-12 mt-12">
+                <div class="relative z-0 w-full mb-6 group ml-6 mt-12">
                     <input type="text" name="nama" id="nama"
                         class="block py-2.5 px-0 w-9/12 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=" " required />
@@ -44,7 +44,7 @@
                         class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nama</label>
                 </div>
 
-                <div class="relative z-0 w-full mb-6 group ml-12 mt-12">
+                <div class="relative z-0 w-full mb-6 group ml-6 mt-12">
                     <input type="tel" name="nomor" id="nomor"
                         class="block py-2.5 px-0 w-9/12 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         required placeholder=" " pattern="^(^\+62|62|^08)(\d{3,4}-?){2}\d{3,4}$" />
@@ -54,7 +54,7 @@
                 </div>
 
                 <div x-data x-init="flatpickr($refs.datetimewidget, { wrap: true, enableTime: true, dateFormat: 'M j, Y h:i K' });" x-ref="datetimewidget"
-                    class="relative z-0 w-full mb-6 group ml-12 mt-12">
+                    class="relative z-0 w-full mb-6 group ml-6 mt-12">
                     <input x-ref="datetime" type="text" id="datetime" data-input
                         class="block py-2.5 px-0 w-9/12 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=" ">
@@ -73,23 +73,23 @@
             </fieldset>
 
             <div class="">
-                <fieldset class="pt-8 border-b-2 pb-8 ">
+                <fieldset class="pt-8 border-b-2 pb-20 mx-8">
 
-                    <div class="space-y-6 ml-12 mr-12 ">
-                        <h4 class="leading-6 text-gray-900 font-medium text-sm mb-8 ">Pilih Jenis Sampah <span
-                                class="font-light block text-gray-500">(minimal 5 kg)
+                    <div class="space-y-6 ml-6 mr-12 ">
+                        <h4 class="leading-6 text-gray-900 font-medium text-medium mb-8 ">Pilih Jenis Sampah <span
+                                class="font-light block text-gray-500 text-sm">(minimal 5 kg)
                             </span>
                         </h4>
 
 
-                        <div class="grid gap-x-3 border-2 p-4 border-[#A8DFF1] rounded-lg">
+                        <div class="grid gap-x-3 border-2 p-4  rounded-lg">
                             <div class=" relative flex gap-x-3">
                                 <div class="flex h-6 items-center">
                                     <input id="botol" name="botol" type="checkbox"
                                         class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
                                 </div>
                                 <div class="text-sm leading-6">
-                                    <label for="botol" class="font-medium text-blue-900">Botol Plastik</label>
+                                    <label for="botol" class="font-medium text-black">Botol Plastik</label>
                                 </div>
                             </div>
                             <div class="grid gap-6 mb-6 mt-4 md:grid-cols-2 ml-7">
@@ -99,51 +99,52 @@
                                         kg</label>
 
                                     <input type="text" id="harga"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="Rp 5.000" readonly required>
                                 </div>
                                 <div>
                                     <label for="jumlah_botol"
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jumlah</label>
                                     <input type="number" id="jumlah_botol"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         min="5" max="25" value="5" required>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="grid gap-x-3 border-2 p-4 border-[#A8DFF1] rounded-lg">
+                        <div class="grid gap-x-3 border-2 p-4 border-gray-200 rounded-lg">
                             <div class=" relative flex gap-x-3">
                                 <div class="flex h-6 items-center">
                                     <input id="kaleng" name="kaleng" type="checkbox"
                                         class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
                                 </div>
                                 <div class="text-sm leading-6">
-                                    <label for="kaleng" class="font-sm text-gray-900">Kaleng</label>
+                                    <label for="kaleng" class="font-medium ">Kaleng</label>
                                 </div>
                             </div>
 
                             <div class="grid gap-6 mb-6 mt-4 md:grid-cols-2 ml-7">
                                 <div>
                                     <label for="harga"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga /
+                                        class="block mb-2 text-sm font-sm text-gray-900 dark:text-white">Harga /
                                         kg</label>
 
                                     <input type="text" id="harga"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder-gray-400"
                                         placeholder="Rp 5.000" readonly required>
                                 </div>
                                 <div>
                                     <label for="jumlah_botol"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jumlah</label>
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jumlah
+                                        (kg)</label>
                                     <input type="number" id="jumlah_botol"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="bg-white border border-gray-300 text-gray-400 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
                                         min="5" max="25" value="5" required>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="grid gap-x-3 border-2 p-4 border-[#A8DFF1] rounded-lg">
+                        <div class="grid gap-x-3 border-2 p-4  rounded-lg">
                             <div class=" relative flex gap-x-3">
                                 <div class="flex h-6 items-center">
                                     <input id="kardus" name="kardus" type="checkbox"
@@ -161,20 +162,20 @@
                                         kg</label>
 
                                     <input type="text" id="harga"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="Rp 5.000" readonly required>
                                 </div>
                                 <div>
                                     <label for="jumlah_botol"
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jumlah</label>
                                     <input type="number" id="jumlah_botol"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         min="5" max="25" value="5" required>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="grid gap-x-3 border-2 p-4 border-[#A8DFF1] rounded-lg">
+                        <div class="grid gap-x-3 border-2 p-4  rounded-lg">
                             <div class=" relative flex gap-x-3">
                                 <div class="flex h-6 items-center">
                                     <input id="organik" name="organik" type="checkbox"
@@ -192,14 +193,14 @@
                                         kg</label>
 
                                     <input type="text" id="harga"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="Rp 5.000" readonly required>
                                 </div>
                                 <div>
                                     <label for="jumlah_botol"
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jumlah</label>
                                     <input type="number" id="jumlah_botol"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         min="5" max="25" value="5" required>
                                 </div>
                             </div>
--- storage/framework/views/da79366a4b442e92c75810fecc685f73.php
+++ storage/framework/views/da79366a4b442e92c75810fecc685f73.php
@@ -12,6 +12,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link href="resources/css/app.css" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/white_logo.png">
+
 
 
     <!-- Scripts -->
@@ -21,22 +23,21 @@
 <body class="h-screen ">
     <div class="min-h-screen ">
         <header class="absolute w-10 py-2 mx-4 hover:animate-pulse">
-            <a href=""><img src="assets/img/back-arrow.png" alt="gambar"></a>
         </header>
 
-        <div class="min-h-screen flex flex-col sm:justify-center items-center sm:pt-0 bg-gradient-to-r from-cyan-500 to-blue-500">
+        <div
+            class="min-h-screen flex flex-col sm:justify-center items-center sm:pt-0 bg-gradient-to-r from-cyan-500 to-blue-500">
 
             <div class="flex justify-center">
-                <div class="mx-6 justify-center">
+                <div class="mx-5 justify-center">
                     <h2 class="font-bold text-3xl w-full tracking-tight text-white">TrashPorter</h2>
                 </div>
-
             </div>
             <div class="flex justify-center mt-1 px-6">
                 <p class="text-white"> Reduce, reuse, and recycle to prevent further damage to our environment</p>
             </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg ">
+            <div
+                class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden sm:rounded-lg ">
                 <div class="w-24 mx-auto flex justify-center mb-4">
                     <a href="/">
                         <?php if (isset($component)) { $__componentOriginal71c6471fa76ce19017edc287b6f4508c = $component; } ?>
@@ -64,4 +65,5 @@
     </div>
 </body>
 
-</html><?php /**PATH /home/fatiya/Documents/Trashporter/INF206-2023-DY1-TrashPorter/resources/views/layouts/guest.blade.php ENDPATH**/ ?>
+</html>
+<?php /**PATH /home/fatiya/Documents/Trashporter/INF206-2023-DY1-TrashPorter/resources/views/layouts/guest.blade.php ENDPATH**/ ?>
