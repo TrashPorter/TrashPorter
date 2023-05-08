5641f6718fac86a8da17c52adc02f933e514ccc2
AditRizkii <adityarizkiramadhan3@gmail.com>
2023-05-09 00:32:46 +0700

    feat : produk controller

--- database/migrations/2023_05_07_040034_create_produk_orders_table.php
+++ database/migrations/2023_05_07_040034_create_produk_orders_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->integer('user_id');
             $table->date('tanggal');
             $table->integer('jumlah_harga');
+            $table->string('status');
             $table->timestamps();
         });
     }
