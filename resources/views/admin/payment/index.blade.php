abd305306c03c152643091c21c86c8fa23fae117
fatiyaquzza <fatiyaquzzaaa@gmail.com>
2023-05-09 14:05:18 +0700

    feat : change text edit into view

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -66,7 +66,7 @@
                             <td class="px-6 py-4 text-right">
                                 <div class="mx-2">
                                     <a href=""
-                                        class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">Edit</a>
+                                        class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">View</a>
                                     |
                                     <form method="POST" class="inline-block" action=""
                                         onsubmit="return confirm('Apakah Anda Yakin?')">
